4fc455e6478d2b5339f069e547d2a17cf1c3261e
stdio2016 <randomfrank8@gmail.com>
2016-11-29 19:27:44 +0800

    滑鼠互動 mouse interaction

--- js/GameScreen.js
+++ js/GameScreen.js
@@ -2,6 +2,7 @@ function GameScreen(){
 	this.debug = null; // To show debug message
 	this.ball = null; // Ball to test animation
 	this.background = null;
+	this.speed = 5;
 }
 
 GameScreen.prototype.preload = function(){
@@ -10,7 +11,7 @@ GameScreen.prototype.preload = function(){
 	game.load.image('background', 'assets/background.png');
 	game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
 	game.time.advancedTiming = true;
-}
+};
 
 GameScreen.prototype.create = function(){
 	console.log("So don't expect me to make a game");
@@ -18,14 +19,15 @@ GameScreen.prototype.create = function(){
 	this.addDebugText();
 	this.ball = game.add.sprite(Math.random()*(game.width-100), Math.random()*(game.height-100), 'ball');
 	var angle = 2*Math.PI*Math.random();
-	var speed = 5;
+	var speed = this.speed;
 	this.ball.velocity = {x: Math.cos(angle)*speed, y: Math.sin(angle)*speed};
-}
+	game.input.addMoveCallback(this.move, this);
+};
 
 GameScreen.prototype.addDebugText = function(){
-	var style = { font: "32px", fill: "green" };
+	var style = { font: "32px", fill: "black" };
 	this.debug = this.game.add.text(0, 0, "0", style);
-}
+};
 
 GameScreen.prototype.update = function(){
 	this.debug.text = game.time.fps;
@@ -58,4 +60,14 @@ GameScreen.prototype.update = function(){
 	if(ball.y < 0){
 		ball.y = 0;
 	}
-}
+};
+
+GameScreen.prototype.move = function(pointer, x, y){
+	if(pointer.isDown){
+		var dx = (this.ball.x+50) - x;
+		var dy = (this.ball.y+50) - y;
+		var norm = this.speed / Math.sqrt(dx*dx + dy*dy);
+		this.ball.velocity.x = -dx * norm;
+		this.ball.velocity.y = -dy * norm;5
+	}
+};
