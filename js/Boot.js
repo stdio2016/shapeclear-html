6046e0616c65c067635789ef3a9855e03eddef5a
stdio2016 <randomfrank8@gmail.com>
2016-12-01 10:15:52 +0800

    新增載入訊息 Add loading message

--- js/Boot.js
+++ js/Boot.js
@@ -3,7 +3,7 @@
 console = window.console || { log: function(s){} };
 
 // Start my game!
-var game = new Phaser.Game('100','100',Phaser.AUTO);
+var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameDiv');
 var gameScreen = new GameScreen();
 game.state.add("GameScreen", gameScreen);
 game.state.start("GameScreen");
