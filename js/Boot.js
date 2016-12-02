caf7bba152affbed508374c80105aa4ed2f46d57
stdio2016 <randomfrank8@gmail.com>
2016-12-02 11:30:32 +0800

    將程式移出網頁 move script out of index.html

--- js/Boot.js
+++ js/Boot.js
@@ -2,6 +2,15 @@
 // If someone's browser doesn't support console, then make a dummy object
 console = window.console || { log: function(s){} };
 
+// Finished loading phaser.js
+var loading = document.getElementById('loading');
+if (window.Phaser) {
+    loading.style.display = 'none';
+}
+else {
+    loading.innerHTML = "Unable to load phaser.js";
+}
+
 // Start my game!
 var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameDiv');
 var gameScreen = new GameScreen();
