a977326c4af30cbeee428703d26ec416dd4a4c53
stdio2016 <randomfrank8@gmail.com>
2016-12-13 17:28:29 +0800

    取得繪圖器種類 get renderer type

--- js/Boot.js
+++ js/Boot.js
@@ -12,3 +12,18 @@ var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameDiv');
 var gameScreen = new GameScreen();
 game.state.add("GameScreen", gameScreen);
 game.state.start("GameScreen");
+
+// Test the renderer
+var renderer = 'unknown';
+switch (game.renderType) {
+  case Phaser.CANVAS:
+    renderer = 'CANVAS';
+    break;
+  case Phaser.WEBGL:
+    renderer = 'WEBGL';
+    break;
+  case Phaser.HEADLESS:
+    renderer = 'HEADLESS';
+    break;
+}
+localStorage.setItem('ShapeClear.renderer', renderer);
