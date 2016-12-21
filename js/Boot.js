ef4821517559c8f281341d46fb7ce46e779ee9be
stdio2016 <randomfrank8@gmail.com>
2016-12-21 11:32:33 +0800

    可以更改繪圖器種類 can change renderer type

--- js/Boot.js
+++ js/Boot.js
@@ -7,8 +7,31 @@ else {
     loading.innerHTML = "Unable to load phaser.js";
 }
 
+// get renderer config
+
+function getRendererConfig() {
+    var renderer;
+    try {
+        renderer = localStorage.getItem('ShapeClear.renderer');
+        switch (renderer){
+          case "WEBGL":
+          case "CANVAS":
+          case "AUTO":
+          case "HEADLESS":
+            renderer = Phaser[renderer];
+            break;
+          default:
+            renderer = Phaser.AUTO;
+        }
+    }
+    catch (e) {
+        renderer = Phaser.AUTO;
+    }
+    return renderer;
+}
+
 // Start my game!
-var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game('100', '100', getRendererConfig(), 'gameDiv');
 var gameScreen = new GameScreen();
 game.state.add("GameScreen", gameScreen);
 game.state.add("Load", new Load());
