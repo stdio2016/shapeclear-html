ab7b0da4c24faf16e15ea26b73dade62c364d671
stdio2016 <randomfrank8@gmail.com>
2016-12-25 11:19:06 +0800

    可以關閉錯誤訊息 make error message closeable

--- js/Boot.js
+++ js/Boot.js
@@ -38,17 +38,29 @@ game.state.add("Load", new Load());
 game.state.start("Load");
 
 var errobj = null;
-// Add error handler
-window.onerror = function (msg, source, lineno, colno, err) {
+function errorHandler(msg, source, lineno, colno, err) {
     window.onerror = null;
-    document.getElementById('alert').style.display='';
+    document.getElementById('alert').style.visibility = 'visible';
     var des =
       'Shape Clear has encountered an error' + '\n' +
-      msg + '\n' +
-      'source: ' + source + '\n' +
-      'position: ' + lineno + ',' + colno + '\n' +
-      'stack trace: \n' + err.stack;
+      msg + '\n';
+    if (msg.match(/script error/i)) {
+        des += 'see Browser Console for more information';
+    }
+    else {
+        des +=
+          'source: ' + source + '\n' +
+          'position: ' + lineno + ',' + colno + '\n' +
+          'stack trace: \n' + err.stack;
+    }
     document.getElementById('description').innerText = des;
     errobj = err;
-    throw err;
-};
+    document.getElementById('close').addEventListener('click', closeErrorBox);
+}
+
+function closeErrorBox() {
+    document.getElementById('alert').style.visibility = 'hidden';
+}
+
+// Add error handler
+window.onerror = errorHandler;
